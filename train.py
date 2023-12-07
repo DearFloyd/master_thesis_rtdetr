94d12435557fd22f4c0ebfbfadb9feaf3a98db18
DearFloyd <736762731@qq.com>
2023-12-07 05:37:30 +0000

    update train cfg

--- train.py
+++ train.py
@@ -3,16 +3,16 @@ warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    model = RTDETR('ultralytics/cfg/models/rt-detr/rtdetr-l.yaml')
-    model.load('/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/weights/rtdetr-l.pt') # loading pretrain weights
+    model = RTDETR('/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/ultralytics/cfg/models/rt-detr/rtdetr-r18.yaml')
+    model.load('/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/weights/rtdetr_r18vd_5x_coco_objects365_from_paddle.pt') # loading pretrain weights
     model.train(data='/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/dataset/classroom_data.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=500,
-                batch=64,
+                epochs=600,
+                batch=48,
                 workers=4,
                 device='0',
                 # resume='', # last.pt path
                 project='/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/outputs/train',
-                name='rtdetr-l-64bs-500ep',
+                name='rtdetr-r18-coco-objects365-48bs-600ep',
                 )
