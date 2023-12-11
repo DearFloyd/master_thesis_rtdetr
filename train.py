c77d5081bd226f3e5b54a9ae95c230514b26c4c7
DearFloyd <736762731@qq.com>
2023-12-11 04:18:50 +0000

    update train log

--- train.py
+++ train.py
@@ -1,22 +1,25 @@
 import warnings
 warnings.filterwarnings('ignore')
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 from ultralytics import RTDETR, YOLO
 
 if __name__ == '__main__':
     # model = YOLO('/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/ultralytics/cfg/models/yolo-detr/yolov8-detr-C2f-Ortho.yaml')
     # model.load('')
     # model = RTDETR('/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/ultralytics/cfg/models/yolo-detr/yolov8-detr-fasternet.yaml')
-    model = RTDETR('/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/ultralytics/cfg/models/yolo-detr/yolov8-detr-C2f-Ortho.yaml')
-    # model.load('/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/weights/rtdetr_r18vd_5x_coco_objects365_from_paddle.pt') # loading pretrain weights
+    model = RTDETR('/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/ultralytics/cfg/models/yolo-detr/yolov8s-detr-C2f-DCNV2-Dynamic.yaml')
+    # model.load('/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/weights/rtdetr_r18vd_5x_coco_objects365_from_paddle.p/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/ultralytics/cfg/models/yolo-detr/yolov8-detr.yamlt') # loading pretrain weights
     model.train(data='/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/dataset/classroom_data.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=600,
-                batch=1,
-                workers=4,
-                device='0',
+                epochs=500,
+                batch=128,
+                workers=8,
+                device='6',
                 # resume='', # last.pt path
                 project='/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/outputs/train',
-                name='yolov8-detr-C2f-Ortho-60bs-600ep',
-                exist_ok=True
+                name='yolov8s-detr-C2f-DCNV2-Dynamic-128bs-500ep',
+                exist_ok=True,
+                patience=100,
                 )
