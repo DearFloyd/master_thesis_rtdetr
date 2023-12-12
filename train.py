6278a658cd5ec93b75eed9db863a6c57308021d0
DearFloyd <736762731@qq.com>
2023-12-12 15:41:46 +0000

    update train log

--- train.py
+++ train.py
@@ -8,18 +8,21 @@ if __name__ == '__main__':
     # model = YOLO('/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/ultralytics/cfg/models/yolo-detr/yolov8-detr-C2f-Ortho.yaml')
     # model.load('')
     # model = RTDETR('/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/ultralytics/cfg/models/yolo-detr/yolov8-detr-fasternet.yaml')
-    model = RTDETR('/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/ultralytics/cfg/models/yolo-detr/yolov8s-detr-C2f-DCNV2-Dynamic.yaml')
+    model = RTDETR('/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/ultralytics/cfg/models/yolo-detr/yolov8s-detr.yaml')
     # model.load('/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/weights/rtdetr_r18vd_5x_coco_objects365_from_paddle.p/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/ultralytics/cfg/models/yolo-detr/yolov8-detr.yamlt') # loading pretrain weights
     model.train(data='/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/dataset/classroom_data.yaml',
                 cache=False,
                 imgsz=640,
                 epochs=500,
-                batch=128,
+                batch=96,
                 workers=8,
-                device='6',
+                device='2',
                 # resume='', # last.pt path
                 project='/workspace/cv-docker/joey04.li/datasets/master_thesis_rtdetr/outputs/train',
-                name='yolov8s-detr-C2f-DCNV2-Dynamic-128bs-500ep',
+                name='yolov8s-detr-96bs-500ep',
                 exist_ok=True,
                 patience=100,
+                seed=1337,
+                optimizer='SGD',
+                cfg='hyp.yaml',
                 )
